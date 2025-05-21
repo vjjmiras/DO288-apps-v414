82099e10380bc95c6ee44f64243796c83e0bda6b
Vicente J. J. Miras <vjjmiras@gmail.com>
2025-05-21 09:18:49 -0400

    fixing issue with concat

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is app version "$_ENV["APP_VERSION"] . "\n";
+print "This is app version " . $_ENV["APP_VERSION"] . "\n";
 ?>
 
